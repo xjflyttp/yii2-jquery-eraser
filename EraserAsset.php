5091e1e41bc4e805b4195ce29964828f4412c200
xjflyttp <xjflyttp@gmail.com>
2015-10-08 12:44:14 +0800

    init

---
+++ EraserAsset.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace xj\eraser;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author xjflyttp <xjflyttp@gmail.com>
+ */
+class EraserAsset extends AssetBundle
+{
+
+    public $sourcePath = '@bower/jquery-eraser';
+    public $basePath = '@webroot/assets';
+    public $js = ['jquery.eraser.js'];
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # yii2-jquery-eraser
+```
 https://github.com/boblemarin/jQuery.eraser
+```
+
+composer.json
+---------
+```json
+"require": {
+    "xj/yii2-jquery-eraser": "~1.0"
+},
+```
+
+View
+---------
+```php
+xj\tagit\EraserAsset::register($this);
+```
